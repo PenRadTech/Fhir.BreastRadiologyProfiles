2b41ba5e8fc7de9f3d231726e9da6ed62036b936
ROBIN\Kurt <KurtA@Gjoll.com>
2020-04-26 10:25:14 -0500

    .

--- Projects/MFish.bat
+++ Projects/MFish.bat
@@ -1,2 +1,1 @@
-..\..\..\Tools\Eir.MFSH\Projects\MFSH\bin\Debug\netcoreapp3.1\MFSH.exe -I MFSH/Include -b MFSH -p MFSH -o FSH  -D %GraphDir% ..\GraphFiles
-
+..\..\..\Tools\Eir.MFSH\Projects\MFSH\bin\Debug\netcoreapp3.1\MFSH.exe -I MFSH/Include -b MFSH -p MFSH -o FSH -D "%%GraphDir%%" ..\GraphFiles
