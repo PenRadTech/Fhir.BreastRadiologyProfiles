1da22768fba099f4e36650aae82523af0fb21781
ROBIN\Kurt <KurtA@Gjoll.com>
2020-05-07 08:38:02 -0500

    .

--- Projects/MFish.bat
+++ Projects/MFish.bat
@@ -1,1 +1,1 @@
-..\..\..\Tools\Eir.MFSH\Projects\MFSH\bin\Debug\netcoreapp3.1\MFSH.exe -I MFSH/Include -b MFSH -p MFSH -o FSH -D "%%GraphDir%%" ..\GraphFiles -u "http://hl7.org/fhir/us/breast-radiology"
+..\..\..\Tools\Eir.MFSH\Projects\MFSH\bin\Debug\netcoreapp3.1\MFSH.exe mfsh.json
