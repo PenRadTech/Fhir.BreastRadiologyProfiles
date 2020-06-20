370d59edc779d1beb5a7de7ce1744eade181e963
Kurt W. Allen <KurtA@Gjoll.com>
2020-06-20 11:42:36 -0500

    .

---
+++ Projects/BRadLibCS/Include/ObservationMacros/BreastBodyLocationRequired.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using BreastRadLib.%ClassName%Local;
+
+namespace BreastRadLib
+{
+	/// <summary>
+	/// Interface %ClassName%.
+	/// This interface is automatically generated.
+	/// </summary>
+	public interface %InterfaceName% %Interfaces%
+	{
+		//+ Properties
+		//- Properties
+
+		//+ Methods
+		//- Methods
+	}
+}
