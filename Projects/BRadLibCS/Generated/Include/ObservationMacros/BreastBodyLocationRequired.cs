204217241e4b9a83e9e8f982ce12cd3ac9aaf837
Kurt W. Allen <KurtA@Gjoll.com>
2020-07-02 19:18:35 -0500

    .

---
+++ Projects/BRadLibCS/Generated/Include/ObservationMacros/BreastBodyLocationRequired.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace BreastRadLib
+{
+    /*
+	 All code in this class is automatically generated.
+	 */
+
+	/// <summary>
+	/// Interface IBreastBodyLocationRequired.
+	/// </summary>
+	public interface IBreastBodyLocationRequired 
+	{
+		public void GetbodySite(out CodeableConcept value);                                                                                       // CSItemFragment.cs:89
+		public void SetbodySite(CodeableConcept value);                                                                                           // CSItemFragment.cs:90
+	}
+
+
+	/// <summary>
+	/// ExtensionClass for interface IBreastBodyLocationRequired.
+	/// </summary>
+	public static class BreastBodyLocationRequired
+	{
+
+	}
+}
