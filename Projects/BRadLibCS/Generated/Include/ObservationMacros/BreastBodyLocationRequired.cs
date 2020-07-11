08b388b1715ae62d00dad41417e57fc0079e1492
Kurt W. Allen <KurtA@Gjoll.com>
2020-07-10 21:00:05 -0500

    .

--- Projects/BRadLibCS/Generated/Include/ObservationMacros/BreastBodyLocationRequired.cs
+++ Projects/BRadLibCS/Generated/Include/ObservationMacros/BreastBodyLocationRequired.cs
@@ -18,24 +18,8 @@ namespace BreastRadLib
 	/// </summary>
 	public interface IBreastBodyLocationRequired 
 	{
-		CodeableConcept BodySite { get; set; }                                                                                                    // CSItemFragment.cs:192
-		List<Extension> Extension { get; set; }                                                                                                   // CSItemFragment.cs:192
-	}
 
-
-	/// <summary>
-	/// ExtensionClass for interface IBreastBodyLocationRequired.
-	/// </summary>
-	public static class BreastBodyLocationRequiredExtensions
-	{
-
-		public static void GetBodySite(this IBreastBodyLocationRequired t, out CodeableConcept value)                                             // CSItemFragment.cs:206
-		{                                                                                                                                         // CSItemFragment.cs:207
-		    value = t.BodySite as CodeableConcept;                                                                                                // CSItemFragment.cs:208
-		}                                                                                                                                         // CSItemFragment.cs:209
-		public static void SetBodySite(this IBreastBodyLocationRequired t, CodeableConcept value)                                                 // CSItemFragment.cs:210
-		{                                                                                                                                         // CSItemFragment.cs:211
-		    t.BodySite = value;                                                                                                                   // CSItemFragment.cs:212
-		}                                                                                                                                         // CSItemFragment.cs:213
+		void GetBodySite(out CodeableConcept value);                                                                                              // CSItemFragment.cs:189
+		void SetBodySite(CodeableConcept value);                                                                                                  // CSItemFragment.cs:190
 	}
 }
